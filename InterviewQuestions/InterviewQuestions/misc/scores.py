06e1eed617b65f05c23dadac41e91e68931fd8b7
calebkumar <kumarcal@grinnell.edu>
2018-05-30 17:28:15 -0700

    Very close to scores solution

--- InterviewQuestions/InterviewQuestions/misc/scores.py
+++ InterviewQuestions/InterviewQuestions/misc/scores.py
@@ -1,3 +1,6 @@
+import operator
+
+
 def countScores(lowerLimits, upperLimits, scores):
     """
     Given lowerLimits, upperLimits, and scores. For each range, find the number of scores (inclusively) within the range.
@@ -7,35 +10,56 @@ def countScores(lowerLimits, upperLimits, scores):
     results = []
 
     for i in range(len(lowerLimits)):
-        lower = findBSIndex(scores, lowerLimits[i], 0, len(scores) - 1)
-        upper = findBSIndex(scores, upperLimits[i], 0, len(scores) - 1)
-        results.append(upper - lower + 1)
+        lower = findLeastGreater(scores, lowerLimits[i])
+        upper = findLeastGreater(scores, upperLimits[i])
+        results.append(upper - lower)
 
     return results
 
 
-def findBSIndex(nums, target, lower, upper):
-    if lower > upper:
-        # print(upper, lower)
-        if nums[lower] < target:
-            return lower
+def findLeastGreater(nums, target):
+    lower = 0
+    upper = len(nums) - 1
+
+    while lower <= upper:
+        mid = (lower + upper) / 2
+        # print lower, mid, upper
+        if nums[mid] <= target:
+            # Look Right
+            lower = mid + 1
         else:
-            return lower
-    mid = (lower + upper) / 2
-    if nums[mid] == target:
-        return mid
-    elif nums[mid] > target:
-        # Look Left
-        return findBSIndex(nums, target, lower, mid - 1)
-    else:
-        # Look Right
-        return findBSIndex(nums, target, mid + 1, upper)
+            # Look Left
+            upper = mid - 1
+
+    return lower
+
+
+def findGreatestLesser(nums, target):
+    lower = 0
+    upper = len(nums) - 1
+
+    while lower < upper:
+        mid = (lower + upper) / 2
+        # print lower, mid, upper
+        if nums[mid] <= target:
+            # Look Right
+            lower = mid + 1
+        else:
+            # Look Left
+            upper = mid
+
+    return lower
 
 
 if __name__ == '__main__':
     # Binary Search Test
-    nums = [0, 5, 10, 12, 15]
+    nums = [0, 5, 10, 13, 16]
     # print(findBSIndex(nums, 15, 0, len(nums)))
+
+    # findLeastGreaterTests
+    test = [0, 5, 10, 12, 15]
+    # print(findLeastGreater(test, 14))
+    # print(findGreatestLesser(test, 6))
 
     # TESTCASE 1
     lowerLimits = [0, 5, 10]
@@ -45,12 +69,26 @@ if __name__ == '__main__':
     # Expected [3, 4, 2]
 
     # TESTCASE 2
-    lowerLimits = [3, 6, 5]
-    upperLimits = [13, 10, 13]
+    lowerLimits = [3, 6, 5] # 1
+    upperLimits = [13, 10, 13] # 4
     scores = [0, 5, 10, 12, 15]
-
     #   Expected [3, 1, 3]
-    # print(countScores(lowerLimits, upperLimits, scores))
+    print(countScores(lowerLimits, upperLimits, scores))
 
 
 # https://stackoverflow.com/questions/6553970/find-the-first-element-in-a-sorted-array-that-is-greater-than-the-target?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+
+def binarySearch(nums, target, lower, upper):
+    if lower > upper:
+        # print(upper, lower)
+        return -1
+    mid = (lower + upper) / 2
+    if nums[mid] == target:
+        return mid
+    elif nums[mid] > target:
+        # Look Left
+        return binarySearch(nums, target, lower, mid - 1)
+    else:
+        # Look Right
+        return binarySearch(nums, target, mid + 1, upper)
